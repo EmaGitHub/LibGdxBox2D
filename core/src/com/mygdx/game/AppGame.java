f03df337fdc761cdcd0e006010ef5787e997b6f8
emanuelecalvisi <emanuele.calvisi@gmail.com>
2020-05-02 16:53:37 +0200

    safe area Inset for iphone X

--- core/src/com/mygdx/game/AppGame.java
+++ core/src/com/mygdx/game/AppGame.java
@@ -1,19 +1,22 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Screens.HomeScreen;
-import com.mygdx.game.Utils.Global;
+import com.mygdx.game.Utils.GlobalVar;
 
 public class AppGame extends Game {
 
 	float screenWidth, screenHeight;
 	public OrthographicCamera camera;
+	private int scale;
 	public Viewport viewport;
 	public SpriteBatch batch;
 	public Screen homeScreen;
@@ -23,15 +26,22 @@ public class AppGame extends Game {
 		screenWidth = Gdx.graphics.getWidth();
 		screenHeight = Gdx.graphics.getHeight();
 
+		GlobalVar.safeAreaInsetTop = getIOSSafeAreaInsets().x;									// support iPhone X
+		GlobalVar.safeAreaInsetBottom = getIOSSafeAreaInsets().y;
+
 		// Screen aspect ratio definition
-		float PPM = screenWidth / Global.widthInPPM;
-		Global.PPM = PPM;
-		float height = screenHeight / Global.PPM;
-		Global.heightInPPM = height;
+		float PPM = screenWidth / GlobalVar.widthInPPM;
+		GlobalVar.PPM = PPM;
+		this.scale = (int) GlobalVar.SCALE;
+		float height = screenHeight / GlobalVar.PPM;
+		GlobalVar.heightInPPM = height;
 		System.out.println("Display aspect ratio: 12 x "+height);
 
+		Vector2 safeAreaInset = getIOSSafeAreaInsets();
 		this.camera = new OrthographicCamera();
-		this.viewport = new StretchViewport(screenWidth, screenHeight, camera);
+		this.viewport = new StretchViewport(screenWidth,
+				screenHeight + GlobalVar.safeAreaInsetBottom, camera);
+		viewport.setScreenPosition(0,0);
 		viewport.apply();
 		batch = new SpriteBatch();
 
@@ -41,9 +51,21 @@ public class AppGame extends Game {
 		Gdx.app.log("Gdx version", com.badlogic.gdx.Version.VERSION);
 	}
 
+	private Vector2 getIOSSafeAreaInsets() {
+		if (Gdx.app.getType() == Application.ApplicationType.iOS) {
+			try {
+				Class<?> IOSLauncher = Class.forName("com.mygdx.game.IOSLauncher");
+				return (Vector2) IOSLauncher.getDeclaredMethod("getSafeAreaInsets").invoke(null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return new Vector2();
+	}
+
 	@Override
 	public void resize (int width, int height){
-		camera.setToOrtho(false, width/Global.SCALE, height/Global.SCALE);
-		viewport.update(width/(int)Global.SCALE, height/(int)Global.SCALE);
+		camera.setToOrtho(false, width/scale, scale);
+		viewport.update(width/scale, height/scale);
 	}
 }
